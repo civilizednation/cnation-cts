bc7a7225f74563923cb559eb6647f7ebca210ab3
문명국 <civilizednation@gmail.com>
2026-09-19 23:55:05 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "cnation-book-v2";
+const CACHE_VERSION = "cnation-book-v3";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const BOOK_CACHE = `${CACHE_VERSION}-books`;
 const FONT_CACHE = `${CACHE_VERSION}-fonts`;
